a9be458827eda545adaca445948171dc5792102b
Arthur Lima <arthur.alves.lima@live.com>
2022-12-29 18:53:39 -0300

    Criando componentes Gallery, Heading e TextContent

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,18 +1,24 @@
+import { forwardRef } from 'react'
 import SlickSlider, { Settings } from 'react-slick'
 
 import * as S from './styles'
 
 export type SliderSettings = Settings
 
-export type SiderProps = {
+export type SliderProps = {
   children: React.ReactNode
   settings: SliderSettings
 }
 
-const Slider = ({ children, settings }: SiderProps) => (
+const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+  { children, settings },
+  ref
+) => (
   <S.Wrapper>
-    <SlickSlider {...settings}>{children}</SlickSlider>
+    <SlickSlider ref={ref} {...settings}>
+      {children}
+    </SlickSlider>
   </S.Wrapper>
 )
 
-export default Slider
+export default forwardRef(Slider)
