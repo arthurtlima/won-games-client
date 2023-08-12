bbca130c89b8e2c4797a3f2e731f8c1b77c17198
Arthur Lima <arthur.alves.lima@live.com>
2023-08-12 18:45:05 -0300

    feat: conectando a sidebar com o apollo e ajustando teste

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,42 +1,59 @@
+import { ParsedUrlQueryInput } from 'querystring'
+import { useRouter } from 'next/router'
+
+import { useQueryGames } from 'graphql/queries/games'
+import { parseQueryStringToFilter, parseQueryStringToWhere } from 'utils/filter'
+
 import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
-import GameCard, { GameCardProps } from 'components/GameCard'
+import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
 
 import * as S from './styles'
-import { useQueryGames } from 'graphql/queries/games'
 
 export type GamesTemplateProps = {
-  games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { push, query } = useRouter()
+
   const { data, loading, fetchMore } = useQueryGames({
-    variables: { limit: 15 }
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
   })
 
-  const handleFilter = () => {
+  const handleFilter = (items: ParsedUrlQueryInput) => {
+    push({
+      pathname: '/games',
+      query: items
+    })
     return
   }
 
   const handleShowMore = () => {
-    fetchMore({
-      variables: {
-        limit: 15,
-        start: data?.games.length
-      }
-    })
+    fetchMore({ variables: { limit: 15, start: data?.games.length } })
   }
 
   return (
     <Base>
       <S.Main>
-        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        <ExploreSidebar
+          initialValues={parseQueryStringToFilter({
+            queryString: query,
+            filterItems
+          })}
+          items={filterItems}
+          onFilter={handleFilter}
+        />
+
         {loading ? (
-          <p>loading...</p>
+          <p>Loading...</p>
         ) : (
           <section>
             <Grid>
@@ -46,7 +63,7 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
                   title={game.name}
                   slug={game.slug}
                   developer={game.developers[0].name}
-                  img={`http://localhost:1337${game.cover?.url}`}
+                  img={`http://localhost:1337${game.cover!.url}`}
                   price={game.price}
                 />
               ))}
