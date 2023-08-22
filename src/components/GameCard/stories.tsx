bcbd13a064d5b85760491902d083b0168b6bd73b
Arthur Lima <arthur.alves.lima@live.com>
2023-08-22 12:36:44 -0300

    feat: criando o componente CartButton e integrando no GameCard

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,17 +1,24 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { CartContextData } from 'hooks/use-cart'
 
 import GameCard, { GameCardProps } from '.'
 
 export default {
   title: 'GameCard',
   component: GameCard,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
   args: {
     slug: 'population-zero',
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 235,
-    promotionalPrice: 215
+    promotionalPrice: 215,
+    isInCart: () => false
   },
   argTypes: {
     onFav: { action: 'clicked' },
@@ -24,6 +31,16 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const IsInCart: Story<GameCardProps & CartContextData> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+IsInCart.args = {
+  isInCart: () => true
+}
 
 export const WithRibbon: Story<GameCardProps> = (args) => (
   <div style={{ width: '30rem' }}>
