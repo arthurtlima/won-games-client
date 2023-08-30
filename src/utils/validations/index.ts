2699976e2e9fe29b84b83715775026a21640a7f1
Arthur Lima <arthur.alves.lima@live.com>
2023-08-30 19:36:56 -0300

    feat: Criando as validações com o Joi usando TDD

---
+++ src/utils/validations/index.ts
@@ -0,0 +1,47 @@
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import Joi from 'joi'
+
+const fieldsValidations = {
+  username: Joi.string().alphanum().min(5).max(30).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  confirm_password: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({
+      'any.only': 'confirm password does not match with confirm password'
+    })
+}
+
+export type FieldErrors = {
+  [key: string]: string
+}
+
+function getFieldErrors(objError: Joi.ValidationResult) {
+  const errors: FieldErrors = {}
+
+  if (objError.error) {
+    objError.error.details.forEach((err) => {
+      errors[err.path.join('.')] = err.message
+    })
+  }
+
+  return errors
+}
+
+export function signUpValidate(values: UsersPermissionsRegisterInput) {
+  const schema = Joi.object(fieldsValidations)
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
+
+type SignValues = Omit<UsersPermissionsRegisterInput, 'username'>
+export function signInValidate(values: SignValues) {
+  const { email, password } = fieldsValidations
+
+  const schema = Joi.object({ email, password })
+
+  return getFieldErrors(schema.validate(values, { abortEarly: false }))
+}
