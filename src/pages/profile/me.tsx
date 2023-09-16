2e6b7233ac0f33fac73d86f2f7dd7c18d7a8324b
Arthur Lima <arthur.alves.lima@live.com>
2023-09-15 21:54:22 -0300

    fix: Session cacheada na chamada do /profile/me

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,9 +1,15 @@
 import { GetServerSidePropsContext } from 'next'
 
-import FormProfile, { FormProfileProps } from 'components/FormProfile'
 import Profile from 'templates/Profile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
 
 import protectedRoutes from 'utils/protected-routes'
+import { initializeApollo } from 'utils/apollo'
+import {
+  QueryProfileMe,
+  QueryProfileMeVariables
+} from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 
 export default function Me(props: FormProfileProps) {
   return (
@@ -15,12 +21,19 @@ export default function Me(props: FormProfileProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<
+    QueryProfileMe,
+    QueryProfileMeVariables
+  >({
+    query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id as string
+    }
+  })
 
   return {
-    props: {
-      session,
-      username: session?.user?.name,
-      email: session?.user?.email
-    }
+    props: { session, username: data.user?.username, email: data.user?.email }
   }
 }
