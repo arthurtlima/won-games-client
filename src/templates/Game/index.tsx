e9eecb2ffc771f27e2ca593b685c75cbeef604d3
Arthur Lima <arthur.alves.lima@live.com>
2023-09-29 18:02:24 -0300

    feat: Configurando o manifest e ícones para o PWA

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,3 +1,5 @@
+import { NextSeo } from 'next-seo'
+
 import Image from 'next/image'
 
 import Base from 'templates/Base'
@@ -14,6 +16,7 @@ import { HighlightProps } from 'components/Highlight'
 import * as S from './styles'
 
 export type GameTemplateProps = {
+  slug?: string
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
@@ -27,6 +30,7 @@ export type GameTemplateProps = {
 }
 
 const Game = ({
+  slug,
   cover,
   gameInfo,
   gallery,
@@ -39,10 +43,25 @@ const Game = ({
   recommendedGames
 }: GameTemplateProps) => (
   <Base>
+    <NextSeo
+      title={`${gameInfo.title} - Won Games`}
+      description={gameInfo.description}
+      canonical={`https://wongames.willianjusten.com.br/game/${slug}`}
+      openGraph={{
+        url: `https://wongames.willianjusten.com.br/game/${slug}`,
+        title: `${gameInfo.title} - Won Games`,
+        description: gameInfo.description,
+        images: [
+          {
+            url: cover,
+            alt: `${gameInfo.title}`
+          }
+        ]
+      }}
+    />
     <S.Cover>
       <Image src={cover} alt={gameInfo.title} layout="fill" />
     </S.Cover>
-
     <S.Main>
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
