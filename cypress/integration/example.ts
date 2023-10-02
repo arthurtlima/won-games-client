19f48639d96f82b2277075dd8e91b182afae6cf9
Arthur Lima <arthur.alves.lima@live.com>
2023-10-02 18:26:39 -0300

    feat: Configurando React Testing Library

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,23 @@
 /// <reference types="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to google', () => {
+  it.skip('should go to google', () => {
     cy.google()
+  })
+
+  it.skip('should change light/dark theme on willian justen site', () => {
+    cy.visit('https://willianjusten.com.br/')
+
+    cy.findByTitle(/Mudar o Tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/Mudar o Tema/i).click()
+    cy.get('.dark').should('exist')
+  })
+
+  it('should visit won games', ()=> {
+    cy.visit('https://wongames.willianjusten.com.br/')
+
+    cy.findByText(/Esse é um site de estudos!/i).should('exist')
   })
 })
