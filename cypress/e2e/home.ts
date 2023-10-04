70197e77846cc861b7bd79981cac40aef063155a
Arthur Lima <arthur.alves.lima@live.com>
2023-10-04 15:37:20 -0300

    feat: Refatorando: criando comandos customizados

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,23 +4,8 @@ describe('Home Page', () => {
     it('should render home sections', () => {
         // visitar a página
         cy.visit('/')
-
-        cy.get('.slick-slider').within(() => {
-            cy.findByRole('heading', { name: /atlantic fleet/i })
-            cy.findByRole('link', { name: /buy now/i })
-
-            cy.get('.slick-dots > :nth-child(2) > button').click()
-            cy.wait(500)
-
-            cy.findByRole('heading', { name: /freespace 2/i })
-            cy.findByRole('link', { name: /buy now/i })
-
-            cy.get('.slick-dots > :nth-child(3) > button').click()
-            cy.wait(500)
-
-            cy.findByRole('heading', { name: /vambrace: cold soul/i })
-            cy.findByRole('link', { name: /browse games/i })
-
-        })
+     
+        // seleciona os banners
+        cy.shouldRenderBanner()
     })
 })
