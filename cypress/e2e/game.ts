801a95d0bf6bfae58653bc4320fa501b9baf5855
Arthur Lima <arthur.alves.lima@live.com>
2023-10-04 18:31:31 -0300

    feat: Adicionando testes nas sessões game details e showcase na pagina de games

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -12,13 +12,34 @@ describe('Game Page', () => {
      })
 
      // gallery
-     cy.findAllByRole('button', { name: /thumb \-/i}).should('have.length.gt', 0)
+     cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
 
      //content
      cy.getByDataCy('content').within(() => {
-        cy.findByRole('heading', { name: /description/i}).should('exist')
+        cy.findByRole('heading', { name: /description/i }).should('exist')
+     })
+     cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+     // Details
+     cy.getByDataCy('game-details').within(() => {
+        cy.findByRole('heading', { name: /game details/i }).should('exist')
+        cy.findByRole('heading', { name: /developer/i }).should('exist')
+        cy.findByRole('heading', { name: /release date/i }).should('exist')
+        cy.findByRole('heading', { name: /platforms/i }).should('exist')
+        cy.findByRole('heading', { name: /publisher/i }).should('exist')
+        cy.findByRole('heading', { name: /rating/i }).should('exist')
+        cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+
+        cy.findAllByText(/larian studios/i).should('exist')
+        cy.findByText(/aug 1, 2023/i).should('exist')
+        cy.findByRole('img', { name: /windows/i }).should('exist')
+        cy.findByRole('img', { name: /mac/i }).should('exist')
+        cy.findByText(/free/i).should('exist')
+        cy.findByText(/role-playing \/ fantasy \/ turn-based/i).should('exist')
      })
 
-     cy.getByDataCy('content').children().should('have.length.at.least', 2)
+     cy.shouldRenderShowcase({ name: 'Upcoming Games', highlight: true })
+     cy.shouldRenderShowcase({ name: 'You may like these games', highlight: false })
     })
 })
