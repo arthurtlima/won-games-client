d61668cf050b8cf5b63947fd9d7f452ca5457a5d
Arthur Lima <arthur.alves.lima@live.com>
2023-10-04 15:26:22 -0300

    feat: Clicando e esperando uma animação com click e wait

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -8,6 +8,19 @@ describe('Home Page', () => {
         cy.get('.slick-slider').within(() => {
             cy.findByRole('heading', { name: /atlantic fleet/i })
             cy.findByRole('link', { name: /buy now/i })
+
+            cy.get('.slick-dots > :nth-child(2) > button').click()
+            cy.wait(500)
+
+            cy.findByRole('heading', { name: /freespace 2/i })
+            cy.findByRole('link', { name: /buy now/i })
+
+            cy.get('.slick-dots > :nth-child(3) > button').click()
+            cy.wait(500)
+
+            cy.findByRole('heading', { name: /vambrace: cold soul/i })
+            cy.findByRole('link', { name: /browse games/i })
+
         })
     })
 })
