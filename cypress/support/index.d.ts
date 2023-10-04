262e04bde1afc42290c73269ca8e9cb6b8524855
Arthur Lima <arthur.alves.lima@live.com>
2023-10-04 16:32:08 -0300

    feat: Utilizando assertions e criando comando para showcase

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,11 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
+
 declare namespace Cypress {
   interface Chainable {
     /**
@@ -8,10 +13,17 @@ declare namespace Cypress {
      * @example cy.google()
      */
     google(): Chainable<Window>
+    
     /**
      * Custom command to check banner in page
-     * @example cy.google()
+     * @example cy.shouldRenderBanner()
      */
     shouldRenderBanner(): Chainable<Element>
+
+        /**
+     * Custom command to check showcase in page
+     * @example cy.shouldRenderShowcase()
+     */
+        shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
