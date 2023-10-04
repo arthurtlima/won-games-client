71b3759167b3e2749babb4ff453c4e650d8ca23c
Arthur Lima <arthur.alves.lima@live.com>
2023-10-03 23:57:02 -0300

    feat: Inicializando servidor do Next e Cypress com start-server-and-test

--- cypress/e2e/example.ts
+++ cypress/e2e/example.ts
@@ -1,6 +1,6 @@
 /// <reference types="../support/index.d.ts" />
 
-describe('Cypress TS', () => {
+describe.skip('Cypress TS', () => {
   it('should go to google', () => {
     cy.google()
   })
---
+++ cypress/e2e/home.ts
@@ -0,0 +1,8 @@
+/// <reference types="../support/index.d.ts" />
+
+describe('Home Page', () => {
+    it('should render home sections', () => {
+        // visitar a página
+        cy.visit('http://localhost:3000')
+    })
+})
