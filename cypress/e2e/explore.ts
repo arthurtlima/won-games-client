10a9fc75c96c7cfce78b884c00848d2859a3e4a0
Arthur Lima <arthur.alves.lima@live.com>
2023-10-06 16:22:40 -0300

    feat: Clicando para mostrar mais jogos na explore (show more)

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -8,9 +8,11 @@ import {
   } from '../../src/utils/filter/fields'
 
 describe('Explore', () => {
-    it('should render filters columns', () => {
+    before(() => {
         cy.visit('/games')
+    })
 
+    it('should render filters columns', () => {
         cy.findByRole('heading', { name: /sort by price/i }).should('exist')
         cy.findByRole('heading', { name: /^price/i }).should('exist')
         cy.findByRole('heading', { name: /platforms/i }).should('exist')
@@ -23,5 +25,13 @@ describe('Explore', () => {
         cy.getFields(sortFields)
 
         cy.getFields(genreFields)
+    })
+
+    it('should show 15 games and show more games when show more is clicked', () => {
+        cy.getByDataCy('game-card').should('have.length', 15)
+
+        cy.findByRole('button' ,{ name: /show more/i }).click()
+
+        cy.getByDataCy('game-card').should('have.length', 30)
     })
 });
