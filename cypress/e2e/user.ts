c568554e7adf763784ed5c768b0722f8af7e12d7
Arthur Lima <arthur.alves.lima@live.com>
2023-10-09 17:33:36 -0300

    feat: Fazendo o teste para o SignIn

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,12 +3,28 @@
 import { createUser } from "../support/generate"
 
 describe('User Page', () => {
-    it('should sign up', () => {
+    it.skip('should sign up', () => {
         const user = createUser()
         cy.visit('/sign-up')
         cy.signUp(user)
         
         cy.url().should('eq', `${Cypress.config().baseUrl}/`)
         cy.findByText(user.username).should('exist')
+    })
+
+
+    it('should sign in and sign out', () => {
+        cy.visit('/sign-in')
+
+        cy.findAllByPlaceholderText(/email/i).type('e2e@wongames.com');
+        cy.findAllByPlaceholderText(/password/i).type('123456');
+        cy.findByRole('button', { name: /sign in now/i }).click()
+     
+        cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+        cy.findByText('cypress').should('exist').click()
+        cy.findByText(/sign out/i).click()
+        
+        cy.findByText('cypress').should('not.exist')
+        cy.findAllByRole('link', { name: /sign in/i }).should('exist')
     })
 })
