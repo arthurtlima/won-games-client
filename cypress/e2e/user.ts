68d19c9fb7c272c8df17b10765556df6681e93a2
Arthur Lima <arthur.alves.lima@live.com>
2023-10-11 15:10:37 -0300

    feat: Testando as rotas protegidas e os redirects após sign in

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -12,7 +12,6 @@ describe('User Page', () => {
         cy.findByText(user.username).should('exist')
     })
 
-
     it('should sign in and sign out', () => {
         cy.visit('/sign-in')
         cy.signIn()
@@ -22,5 +21,22 @@ describe('User Page', () => {
     
         cy.findByRole('link', { name: /sign in/i }).should('exist')
         cy.findByText(/cypress/i).should('not.exist')
-      });
+    });
+
+    it.only('should sign the user and redirect to the page that it was defined previously', () => {
+        cy.visit('/profile/me')
+       
+        // redirecionando para o sign-in com a callback do profile/me
+        cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`)
+        
+        // fazer o sign in
+        cy.signIn()
+
+        // espero ser redirecionado para o profile
+        cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/me`)
+
+        cy.findByLabelText(/username/i).should('have.value', 'cypress')
+        cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com')
+    })
 })
+
